c76e84da617bd23b3d949d2518b37b988a92f18c
pixelbreaker <pixelbraker@gmail.com>
2011-06-13 18:05:57 +0100

    streamlined

--- takeoverloader/jquery.takeoverloader-0.1.js
+++ takeoverloader/jquery.takeoverloader-0.1.js
@@ -2,7 +2,7 @@
  * jQuery Image Take Over loader v0.1
  * http://blog.pixelbreaker.com/javascript/takeover-loader
  *
- * Copyright (c) 2009-2010 Gabriel Bucknall
+ * Copyright (c) 2011 Gabriel Bucknall
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://jquery.org/license
  *
@@ -14,49 +14,29 @@
 	$.fn.takeoverloader = function( progress, complete )
 	{
 		var
-			el = this,
-			images_ = new Array(),
-			numImages_,
+			eligible = this.find('img:not([src=""]):not(.noloader)'),
+			total_ = eligible.length-1,
+			loaded_ = 0,
 			progress_ = progress || function(){},
 			complete_ = complete || function(){};
 			
-		var getAllImages = function()
+		eligible.each(function()
 		{
-			el.find('img:not([src=""]):not(.noloader)').each(function()
-			{
-				images_.push( {uri: $(this).attr('src'), el:this} );
-			});
-			numImages_ = images_.length;
-		};
+			if( this.complete ) 
+				stepProgress();
+			else 
+				$(this).load( function(){stepProgress();} );
+		});
 
-		var addListeners = function()
+		var stepProgress = function()
 		{
-			$.each( images_, function( k, v )
+			loaded_++;
+			progress_( loaded_/total_ );
+			if(loaded_>=total_)
 			{
-				var imgObj = $(v.el);
-				if( v.el.complete )
-					addLoaded();
-				else
-					$(v.el).load(function()
-					{
-						addLoaded();
-					});
-			});
-		};
-
-		var addLoaded = function()
-		{
-			numImages_--;
-			progress_( 1-(numImages_/images_.length) );
-			if(numImages_==0)
-			{
-				$.each( images_, function( k, v ){ $(v.el).unbind( 'load' ); });
-
+				eligible.unbind( 'load' );
 				complete_();
 			}
 		};
-
-		getAllImages();
-		addListeners();
 	}
 })( jQuery );
