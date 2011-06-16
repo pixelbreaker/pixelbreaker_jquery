c31417ade345859006539f0cfdce3fe76f4c8253
pixelbreaker <pixelbraker@gmail.com>
2011-06-16 23:58:01 +0100

    typo.

--- takeoverloader/example.js
+++ takeoverloader/example.js
@@ -3,17 +3,21 @@ $(document).ready( function()
 	// hide content div (UI being loaded)
 	$('#content').hide();
 	// takeover the loading.
-	$(document).takeoverloader( 
-		function( progressAmount ) // called each time an item is loaded
-		{
-			// do something with progressAmout (normalised)
-			$('.loadprogress').text( Math.round( progressAmount * 100 )+'%' );
-		},
-		function() // called when loading is finished
-		{
-			$('.loadprogress').hide();
-			$('#content').fadeIn();
-			// etc
+	$(document).takeoverloader( {
+		progress: function( progressAmount ) // called each time an item is loaded
+			{
+				// do something with progressAmout (normalised)
+				$('.loadprogress').text( Math.round( progressAmount * 100 )+'%' );
+			},
+		complete: function() // called when loading is finished
+			{
+				$('.loadprogress').hide();
+				$('#content').fadeIn();
+				// etc
+			}
 		}
 	);
+		
+	// To forcibly remove events, call 'destroy'
+	$(document).takeoverloader( 'destroy' );
 });
--- takeoverloader/jquery.takeoverloader-0.1.js
+++ takeoverloader/jquery.takeoverloader-0.1.js
@@ -83,7 +83,7 @@
 		} else if ( typeof method === 'object' || ! method ) {
 			return methods.init.apply( this, arguments );
 		} else {
-			$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+			$.error( 'Method ' +  method + ' does not exist on jQuery.takeoverloader' );
 		}
 	}
 })( jQuery );
